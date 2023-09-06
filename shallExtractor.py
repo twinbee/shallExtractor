63191bb4fea6435bd89c381823cc521de0208216
Matthew Bennett <matt.bennett@missecurity.com>
2023-09-06 12:50:29 -0500

    not sure, its been a while

--- shallExtractor.py
+++ shallExtractor.py
@@ -20,22 +20,37 @@ def extract_paragraphs_with_word(input_file, output_file, search_word='shall'):
     sections = re.split(section_pattern, pdf_text)[1:]  # Skip the first element (empty)
 
     rows = []
+    page_number = 0  # Initialize page number
+
     for i in range(0, len(sections), 3):
         section_info = sections[i]
         section_desc = sections[i + 1] if i + 1 < len(sections) else ""
 
         sentences = sent_tokenizer.tokenize(sections[i + 2])
         relevant_sentences = []
+        current_sentence = ""
+
         for sentence in sentences:
+            if re.search(r'\b\d+\b', sentence):
+                page_number = re.search(r'\b\d+\b', sentence).group()  # Update page number
             if search_word in sentence.lower():
-                relevant_sentences.append(sentence.replace('\n', ' ').replace('  ', ' '))  # Remove line feeds and double spaces
+                if current_sentence:
+                    current_sentence += ' ' + sentence
+                else:
+                    current_sentence = sentence
+            elif current_sentence:
+                relevant_sentences.append(current_sentence)
+                current_sentence = ""
+        
+        if current_sentence:
+            relevant_sentences.append(current_sentence)
 
         for sentence in relevant_sentences:
-            rows.append([section_info, section_desc, sentence.strip()])
+            rows.append([section_info, section_desc, sentence.replace('\n', ' ').replace('\r', ' ').replace('  ', ' ').strip(), page_number])
 
     with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['Section Info', 'Nearest Subheading', 'Sentence with "{}"'.format(search_word)])
+        csvwriter.writerow(['Section Info', 'Nearest Subheading', 'Sentence with "{}"'.format(search_word), 'Page Number'])
         for row in rows:
             csvwriter.writerow(row)
 
